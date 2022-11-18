b17e42b112c4b6073b07a9d4bef2e60a1da506d4
Devrath <devrath@mplgaming.com>
2022-11-18 20:54:42 +0530

    more code

--- Code/app/src/main/java/com/droid/demo/api/FocusApiActivity.kt
+++ Code/app/src/main/java/com/droid/demo/api/FocusApiActivity.kt
@@ -8,11 +8,22 @@ import com.droid.demo.databinding.ActivityApiFocusBinding
 class FocusApiActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityApiFocusBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityApiFocusBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.showKeyboardId.setOnClickListener {
+            binding.inputActionId.requestFocus()
+        }
+
+        binding.showKeyboardId.setOnClickListener {
+            window.decorView.clearFocus();
+        }
     }
 }
--- Code/app/src/main/java/com/droid/demo/api/InputFocusManagerApiActivity.kt
+++ Code/app/src/main/java/com/droid/demo/api/InputFocusManagerApiActivity.kt
@@ -1,12 +1,82 @@
 package com.droid.demo.api
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.inputmethod.InputMethodManager
 import com.droid.demo.R
+import com.droid.demo.databinding.ActivityApiFocusBinding
+import com.droid.demo.databinding.ActivityApiInputFocusManagerBinding
 
 class InputFocusManagerApiActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityApiInputFocusManagerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_api_input_focus_manager)
+        binding = ActivityApiInputFocusManagerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.showKeyboardId.setOnClickListener {
+            /*it.post {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.inputActionId.windowToken,InputMethodManager.SHOW_IMPLICIT)
+            }*/
+            binding.inputActionId.requestFocus()
+            binding.inputActionId.post {
+                if(binding.inputActionId.hasFocus()){
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(it, InputMethodManager.SHOW_IMPLICIT)
+                }
+            }
+
+        }
+
+        binding.hideKeyboardId.setOnClickListener {
+
+        }
+    }
+
+
+    fun View.focusAndShowKeyboard() {
+        /**
+         * This is to be called when the window already has focus.
+         */
+        fun View.showTheKeyboardNow() {
+            if (isFocused) {
+                post {
+                    // We still post the call, just in case we are being notified of the windows focus
+                    // but InputMethodManager didn't get properly setup yet.
+                    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+                }
+            }
+        }
+
+        requestFocus()
+        if (hasWindowFocus()) {
+            // No need to wait for the window to get focus.
+            showTheKeyboardNow()
+        } else {
+            // We need to wait until the window gets focus.
+            viewTreeObserver.addOnWindowFocusChangeListener(
+                object : ViewTreeObserver.OnWindowFocusChangeListener {
+                    override fun onWindowFocusChanged(hasFocus: Boolean) {
+                        // This notification will arrive just before the InputMethodManager gets set up.
+                        if (hasFocus) {
+                            this@focusAndShowKeyboard.showTheKeyboardNow()
+                            // It’s very important to remove this listener once we are done.
+                            viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                        }
+                    }
+                })
+        }
     }
 }
